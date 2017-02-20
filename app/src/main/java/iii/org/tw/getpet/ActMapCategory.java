d18ecdea36452bb4293053dcf1b0fceb0eca4a68
sychiang <chris.chiang13@gmail.com>
2017-02-20 12:21:55 +0800

    v.170220_03_地圖搜尋定位功能OK

--- app/src/main/java/iii/org/tw/getpet/ActMapCategory.java
+++ app/src/main/java/iii/org/tw/getpet/ActMapCategory.java
@@ -1,23 +1,39 @@
 package iii.org.tw.getpet;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import common.CDictionary;
 
 public class ActMapCategory extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_map_category);
         initComponent();
+        //**
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED ||checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 102);
+        }
+
+
+        //**
+
     }
 
     @Override
--- app/src/main/java/iii/org/tw/getpet/ActMapSearch.java
+++ app/src/main/java/iii/org/tw/getpet/ActMapSearch.java
@@ -1,21 +1,28 @@
 package iii.org.tw.getpet;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.text.Html;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -31,6 +38,7 @@ import model.object_MapMarkData;
 
 public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final int MY_LOCATION_REQUEST_CODE = 2001;
     CountDownLatch l_CountDownLatch;
     ArrayList<object_MapMarkData> l_arrayList_object_MapMarkData;
     Gson gson = new Gson();
@@ -39,17 +47,24 @@ public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallbac
     Double gv_dblLat, gv_dblLon;
     Intent intent;
     String mapType;
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //**
+        //ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        //**
+
         intent = getIntent();
         mapType = intent.getExtras().getString(CDictionary.BK_mapType);
-        url += "?$filter=mapType eq '"+mapType+"'";
+        url += "?$filter=mapType eq '" + mapType + "'";
         setContentView(R.layout.act_map_search);
         setTitle(mapType);
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map); //取得地圖
         mapFragment.getMapAsync(this);
+
 
         l_CountDownLatch = new CountDownLatch(1);
 
@@ -75,8 +90,54 @@ public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
+    //*
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+            } else {
+                // Permission was denied. Display an error message.
+            }
+        }
+    }
+    //*
+
+
+
     @Override
     public void onMapReady(final GoogleMap googleMap) {
+
+        //**
+
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.d("地圖設置為真","123");
+            googleMap.setMyLocationEnabled(true);
+        } else {
+            // Show rationale and request permission.
+        }
+
+
+
+
+
         if (true/*gv_dblLat != null && gv_dblLon != null*/) {
 
             //**
@@ -118,26 +179,61 @@ public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallbac
 
             //**
 
-            MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.position(new LatLng(22.628395, 120.292995));
-            markerOptions.title("Im title"/*intent.getExtras().getString(CDictionary.BK_shelter_name)*/);
-            markerOptions.draggable(false);
-            googleMap.addMarker(markerOptions);
 
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(markerOptions.getPosition()));
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(markerOptions.getPosition())
-                    .zoom(17)
-                    .build();
-            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//            MarkerOptions markerOptions = new MarkerOptions();
+//            markerOptions.position(new LatLng(22.628395, 120.292995));
+//            markerOptions.title("Im title"/*intent.getExtras().getString(CDictionary.BK_shelter_name)*/);
+//            markerOptions.draggable(false);
+//            googleMap.addMarker(markerOptions);
 
+
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(markerOptions.getPosition()));
+//            CameraPosition cameraPosition = new CameraPosition.Builder()
+//                    .target(markerOptions.getPosition())
+//                    .zoom(17)
+//                    .build();
+//            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            //**
+            LocationManager locationManager = (LocationManager)
+                    getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            double latitude = 22.628228;
+            double longitude = 120.2908483;
+
+
+            Location locationNet = locationManager.getLastKnownLocation(locationManager
+                    .NETWORK_PROVIDER);
+            Location locationGps = locationManager.getLastKnownLocation(locationManager
+                    .GPS_PROVIDER);
+            if(locationGps!=null){
+                latitude = locationGps.getLatitude();
+                longitude = locationGps.getLongitude();
+
+            }else if(locationNet != null){
+                latitude = locationNet.getLatitude();
+                longitude = locationNet.getLongitude();
+            }
+
+
+
+
+            //**
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                //如果GPS或網路定位開啟，呼叫locationServiceInitial()更新位置
+
+
+            } else {
+                Toast.makeText(this, "請開啟定位服務", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));	//開啟設定頁面
+            }
             //**
 
             MarkerOptions markerOptions2 = new MarkerOptions();
-            markerOptions2.position(new LatLng(22.628228, 120.2908483));
-            markerOptions2.title("南區資策會"/*intent.getExtras().getString(CDictionary.BK_shelter_name)*/);
-            markerOptions2.draggable(false);
-            googleMap.addMarker(markerOptions2);
+            markerOptions2.position(new LatLng(latitude, longitude));
+            //markerOptions2.title("我的位置"/*intent.getExtras().getString(CDictionary.BK_shelter_name)*/);
+            //markerOptions2.draggable(false);
+            //googleMap.addMarker(markerOptions2);
 
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(markerOptions2.getPosition()));
             CameraPosition cameraPosition2 = new CameraPosition.Builder()
@@ -146,8 +242,10 @@ public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallbac
                     .build();
 
             //***
+
             googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition2));
             //**
+
         }
 
 
