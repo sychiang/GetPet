4d6682ef475fafa517ef6a2daa88b6ca7ed35b7f
sychiang <chris.chiang13@gmail.com>
2017-02-22 22:37:36 +0800

    v.170222_07_地圖搜尋修正(未測試)

--- app/src/main/java/iii/org/tw/getpet/ActMapSearch.java
+++ app/src/main/java/iii/org/tw/getpet/ActMapSearch.java
@@ -36,10 +36,13 @@ import common.CDictionary;
 import cz.msebera.android.httpclient.Header;
 import model.object_MapMarkData;
 
+//**
+//**
+
 public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final int MY_LOCATION_REQUEST_CODE = 2001;
-    CountDownLatch l_CountDownLatch;
+    CountDownLatch iv_CountDownLatch;
     ArrayList<object_MapMarkData> l_arrayList_object_MapMarkData;
     Gson gson = new Gson();
     String url = "http://twpetanimal.ddns.net:9487/api/v1/maps";
@@ -65,8 +68,7 @@ public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallbac
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map); //取得地圖
         mapFragment.getMapAsync(this);
 
-
-        l_CountDownLatch = new CountDownLatch(1);
+        iv_CountDownLatch = new CountDownLatch(1);
 
         loopjClient.get(ActMapSearch.this, url, new AsyncHttpResponseHandler() {
             @Override
@@ -77,7 +79,7 @@ public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallbac
                 }.getType());
                 Log.d("Debug", "" + l_arrayList_object_MapMarkData.get(0).getMaplatitude());
 
-                l_CountDownLatch.countDown();
+                iv_CountDownLatch.countDown();
                 Log.d("Debug22", "" + l_arrayList_object_MapMarkData.get(0).getMaplatitude());
             }
 
@@ -118,77 +120,19 @@ public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(final GoogleMap googleMap) {
 
-        //**
 
-
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            Log.d("地圖設置為真","123");
-            googleMap.setMyLocationEnabled(true);
-        } else {
-            // Show rationale and request permission.
-        }
-
-
-
-
-
-        if (true/*gv_dblLat != null && gv_dblLon != null*/) {
-
-            //**
-
-            final Thread l_thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-
-                    try {
-                        l_CountDownLatch.await();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    for (final object_MapMarkData obj : l_arrayList_object_MapMarkData) {
-                        Log.d("Debug1", "" + obj.getMaplatitude());
-                        if (obj.getMaplatitude() != null) {
-                            final LatLng gps = new LatLng(Double.valueOf(obj.getMaplatitude()), Double.valueOf(obj.getMaplongitude()));
-
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    MarkerOptions markerOptions = new MarkerOptions();
-                                    markerOptions.position(gps);
-                                    markerOptions.title(obj.getMapName());
-                                    markerOptions.snippet(obj.getMapContent());
-                                    markerOptions.draggable(false);
-                                    googleMap.addMarker(markerOptions);
-                                }
-                            });
-
-                        }
-
-                    }
-                }
-            });
-
-            l_thread.start();
-
+        try {
             //**
 
 
-//            MarkerOptions markerOptions = new MarkerOptions();
-//            markerOptions.position(new LatLng(22.628395, 120.292995));
-//            markerOptions.title("Im title"/*intent.getExtras().getString(CDictionary.BK_shelter_name)*/);
-//            markerOptions.draggable(false);
-//            googleMap.addMarker(markerOptions);
 
-
-//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(markerOptions.getPosition()));
-//            CameraPosition cameraPosition = new CameraPosition.Builder()
-//                    .target(markerOptions.getPosition())
-//                    .zoom(17)
-//                    .build();
-//            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.d("地圖設置為真","123");
+                googleMap.setMyLocationEnabled(true);
+            } else {
+                // Show rationale and request permission.
+            }
 
             //**
             LocationManager locationManager = (LocationManager)
@@ -197,28 +141,48 @@ public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallbac
             double latitude = 22.628228;
             double longitude = 120.2908483;
 
+            //**
 
-            Location locationNet = locationManager.getLastKnownLocation(locationManager
-                    .NETWORK_PROVIDER);
-            Location locationGps = locationManager.getLastKnownLocation(locationManager
-                    .GPS_PROVIDER);
-            if(locationGps!=null){
-                latitude = locationGps.getLatitude();
-                longitude = locationGps.getLongitude();
+            //LocationManager manager=(LocationManager)getSystemService(LOCATION_SERVICE);
+            Log.d("GPS定位是否開啟",locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)+"");
+            Log.d("NETWORK定位是否開啟",locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)+"");
+            if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||
+                    locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+            {
+                Location gps=locationManager.getLastKnownLocation("network");
+                if(gps!=null){
+                    StringBuffer message=new StringBuffer();
+                    message.append("位置：緯度:");
+                    message.append(Double.toString(gps.getLongitude()));
+                    message.append(",經度：");
+                    message.append(Double.toString(gps.getLatitude()));
+                    Log.d("取到的經緯度",message.toString());
+                    longitude = gps.getLongitude();
+                    latitude = gps.getLatitude();
 
-            }else if(locationNet != null){
-                latitude = locationNet.getLatitude();
-                longitude = locationNet.getLongitude();
-            }
+                }
+                else
+                    Toast.makeText(this,"定位不到目前位置",Toast.LENGTH_LONG).show();
+            }else
+                Toast.makeText(this,"沒有開啟定位功能",Toast.LENGTH_LONG).show();
 
+            //**
+        /*
+        Location locationNet = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+        Location locationGps = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
 
+        if(locationGps!=null){
+            latitude = locationGps.getLatitude();
+            longitude = locationGps.getLongitude();
 
-
+        }else if(locationNet != null){
+            latitude = locationNet.getLatitude();
+            longitude = locationNet.getLongitude();
+        }
+*/
             //**
             if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
                 //如果GPS或網路定位開啟，呼叫locationServiceInitial()更新位置
-
-
             } else {
                 Toast.makeText(this, "請開啟定位服務", Toast.LENGTH_LONG).show();
                 startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));	//開啟設定頁面
@@ -227,7 +191,7 @@ public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallbac
 
             MarkerOptions markerOptions2 = new MarkerOptions();
             markerOptions2.position(new LatLng(latitude, longitude));
-            //markerOptions2.title("我的位置"/*intent.getExtras().getString(CDictionary.BK_shelter_name)*/);
+            markerOptions2.title("我的位置"/*intent.getExtras().getString(CDictionary.BK_shelter_name)*/);
             //markerOptions2.draggable(false);
             //googleMap.addMarker(markerOptions2);
 
@@ -236,15 +200,58 @@ public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallbac
                     .target(markerOptions2.getPosition())
                     .zoom(15)
                     .build();
-
             //***
-
             googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition2));
+            //iv_CountDownLatch.countDown();
             //**
 
+            if (true/*gv_dblLat != null && gv_dblLon != null*/) {
+                //**
+                final Thread l_thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            iv_CountDownLatch.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        for (final object_MapMarkData obj : l_arrayList_object_MapMarkData) {
+                            Log.d("Debug1", "" + obj.getMaplatitude());
+                            if (obj.getMaplatitude() != null) {
+                                final LatLng gps = new LatLng(Double.valueOf(obj.getMaplatitude()), Double.valueOf(obj.getMaplongitude()));
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        MarkerOptions markerOptions = new MarkerOptions();
+                                        markerOptions.position(gps);
+                                        markerOptions.title(obj.getMapName());
+                                        markerOptions.snippet(obj.getMapContent());
+                                        markerOptions.draggable(false);
+                                        googleMap.addMarker(markerOptions);
+                                    }
+                                });
+                            }
+                        }
+                    }
+                });
+                l_thread.start();
+                //**
+//            MarkerOptions markerOptions = new MarkerOptions();
+//            markerOptions.position(new LatLng(22.628395, 120.292995));
+//            markerOptions.title("Im title"/*intent.getExtras().getString(CDictionary.BK_shelter_name)*/);
+//            markerOptions.draggable(false);
+//            googleMap.addMarker(markerOptions);
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(markerOptions.getPosition()));
+//            CameraPosition cameraPosition = new CameraPosition.Builder()
+//                    .target(markerOptions.getPosition())
+//                    .zoom(17)
+//                    .build();
+//            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
         }
-
-
+        catch (Exception ex) {
+            Toast.makeText(this,"請確認網路訊號是否良好",Toast.LENGTH_LONG).show();
+        }
     }
 
     public void getDataFromServer() {
@@ -258,7 +265,7 @@ public class ActMapSearch extends AppCompatActivity implements OnMapReadyCallbac
                 l_arrayList_object_MapMarkData = gson.fromJson(responsestr, new TypeToken<ArrayList<object_MapMarkData>>() {
                 }.getType());
                 Log.d("Debug", "" + l_arrayList_object_MapMarkData.get(0).getMaplatitude());
-                l_CountDownLatch.countDown();
+                iv_CountDownLatch.countDown();
                 Log.d("Debug22", "" + l_arrayList_object_MapMarkData.get(0).getMaplatitude());
             }
 
